8a3bce316e5f12340aeab39a4d9f2431ec75cd20
Nic DeVos <nic@goodrx.com>
2019-03-01 15:53:40 -0700

    Better purple color for hostname in prompt

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -2,5 +2,5 @@
 # prompt
 #
 
-export PS1="[${COLOR_DEEP_SKY_BLUE1}\u${COLOR_RESET}@${COLOR_MEDIUM_ORCHID1}\h${COLOR_RESET}:${COLOR_YELLOW1}\w${COLOR_RESET}]\$(__git_prompt)\n\$ "
+export PS1="[${COLOR_DEEP_SKY_BLUE1}\u${COLOR_RESET}@${COLOR_MEDIUM_PURPLE2}\h${COLOR_RESET}:${COLOR_YELLOW1}\w${COLOR_RESET}]\$(__git_prompt)\n\$ "
 
