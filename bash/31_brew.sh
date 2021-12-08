1d43f98f50a614b6252f8201da3b125eb0870a7f
Nic DeVos <nic@goodrx.com>
2021-12-08 15:42:39 -0700

    Better bash completion

--- bash/31_brew.sh
+++ bash/31_brew.sh
@@ -6,17 +6,18 @@ __which_quietly brew || return 0
 
 eval "$(brew shellenv)"
 
-# completion setup for `bash`
-__bash_source_if_present "$(brew --prefix)/etc/bash_completion"
-
-for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
-  __bash_source_if_present "$COMPLETION"
-done
+if [[ -f "$HOMEBREW_PREFIX" ]]; then
+  __bash_source_if_present "${HOMEBREW_PREFIX}/etc/bash_completion"
+else
+  for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+    __bash_source_if_present "$COMPLETION"
+  done
+fi
 
 # add brew include dirs for better compilation
-export CFLAGS="-I$(brew --prefix)/include -L$(brew --prefix)/lib"
+export CFLAGS="-I${HOMEBREW_PREFIX}/include -L${HOMEBREW_PREFIX}/lib"
 # use updated openssl libs if available
-if [[ -d "$(brew --prefix)/opt/openssl" ]]; then
-  export LDFLAGS="-I$(brew --prefix)/opt/openssl/include -L$(brew --prefix)/opt/openssl/lib"
+if [[ -d "${HOMEBREW_PREFIX}/opt/openssl" ]]; then
+  export LDFLAGS="-I${HOMEBREW_PREFIX}/opt/openssl/include -L${HOMEBREW_PREFIX}/opt/openssl/lib"
 fi
 
