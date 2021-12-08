10eecf83d01c3116d1731f35d19c7c31d5aea9b3
Nic DeVos <nic@devos.me>
2021-12-08 11:52:12 -0700

    Add brew shell command completion

--- bash/31_brew.sh
+++ bash/31_brew.sh
@@ -6,6 +6,10 @@ __which_quietly brew || return 1
 
 eval "$(brew shellenv)"
 
+for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+  [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+done
+
 # add brew include dirs for better compilation
 export CFLAGS="-I$(brew --prefix)/include -L$(brew --prefix)/lib"
 # use updated openssl libs if available
