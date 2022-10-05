b2655e4b2905ad8a873c846716892de27a71f4ab
Nic DeVos <nic@devos.me>
2022-10-05 08:50:55 -0600

    Add -h to `ls` aliases

--- bash/20_aliases.sh
+++ bash/20_aliases.sh
@@ -6,9 +6,9 @@ if __is_os Linux; then
   # tell `ls` to use colors. Also see `$CLICOLOR`
   alias ls='ls --color=auto'
 fi
-alias l='ls -l'
-alias ll='ls -l'
-alias lal='ls -al'
+alias l='ls -lh'
+alias ll='ls -lh'
+alias lal='ls -alh'
 alias ..='cd ../'
 alias ...='cd ../../'
 alias ....='cd ../../../'
